0ef545cb39d55ca7627b86014131d2a8360ca096
Ty5554 <b.boowy1002@gmail.com>
2024-12-11 20:56:17 +0900

    Add OGP

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem "bootsnap", require: false
 
 gem "devise"
 
+gem "meta-tags"
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'SwimFirst',
+      title: 'SwimFirst',
+      reverse: true,
+      charset: 'utf-8',
+      description: '水泳選手のためのコンディション管理サービス',
+      keywords: '水泳,コンディション,トレーニング',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url("swimerLogo.png") },
+        { href: image_url("ogp.png"), rel: "apple-touch-icon", sizes: "90x90", type: "image/png" },
+      ],
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'), 
+        local: 'ja-JP'
+      }
+    }
+  end
 end
