030e15c4052180d52241ba8dde5ce1918e62206a
Ty5554 <b.boowy1002@gmail.com>
2024-12-22 21:42:18 +0900

    Add searchform to body(athlete/coach)

--- app/controllers/bodies_controller.rb
+++ app/controllers/bodies_controller.rb
@@ -4,10 +4,14 @@ class BodiesController < ApplicationController
     before_action :authorize_approved, only: %i[index new create edit update show edit update destroy]
 
     def index
-      @bodies = current_user.bodies.page(params[:page]).per(3)
-      @all_bodies = ::Body.joins(user: :teams)
-      .where(teams: { id: current_user.teams.ids })
-      .distinct.page(params[:page]).per(3)
+      @q = current_user.bodies.page(params[:page]).per(3).ransack(params[:q])
+      @bodies = @q.result(distinct: true)
+      @all_q = ::Body.joins(user: :teams)
+                        .where(teams: { id: current_user.teams.ids })
+                        .distinct
+                        .page(params[:page]).per(3)
+                        .ransack(params[:all_q])
+      @all_bodies = @all_q.result(distinct: true)
     end
 
     def new
